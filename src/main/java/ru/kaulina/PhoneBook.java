388fcd112d851546da8ed371dcd06b80101a3f3e
Kaulina <iv_kaulina@mail.ru>
2024-05-28 22:12:25 +0700

    Stub and tests for add

--- src/main/java/ru/kaulina/PhoneBook.java
+++ src/main/java/ru/kaulina/PhoneBook.java
@@ -1,5 +1,18 @@
 package ru.kaulina;
 
 public class PhoneBook {
+    private static PhoneBook INSTANCE = null;
 
+    private PhoneBook(){}
+
+    public static PhoneBook getINSTANCE(){
+        if(INSTANCE == null){
+            INSTANCE = new PhoneBook();
+        }
+        return INSTANCE;
+    }
+
+    public int add(String name, String namberPhone){
+        return  0;
+    }
 }
